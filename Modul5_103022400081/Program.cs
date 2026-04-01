74aaaa6679d950f0355b4528bacbaec3dbb3df12
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-01 07:30:50 +0700

    implementasi-generic-method

--- Modul5_103022400081/Program.cs
+++ Modul5_103022400081/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("MODUL 5");
+using Modul5_103022400081;
+using System;
+
+namespace Modul5_10302240081
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            PemrosesData nilai = new PemrosesData();
+            float terbesar = nilai.DapatkanNilaiTerbesar(10, 30, 22);
+            Console.WriteLine("nilai terbesar: " + terbesar);
+        }
+    }
+    
+}
