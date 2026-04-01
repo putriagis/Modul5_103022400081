901659ff9606b9acf2c9b73bc35ede1fa1b15d8a
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-01 07:56:59 +0700

    menambahkan class SimpleDataBase

--- Modul5_103022400081/Program.cs
+++ Modul5_103022400081/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("MODUL 5");
+using System;
+namespace Modul5_103022400081;
+
+class program
+{
+    private static void Main(string[] args)
+    {
+        SimpleDataBase<int> Database = new SimpleDataBase<int>();
+        Database.addNewData(81);
+        Database.addNewData(00);
+        Database.addNewData(40);
+        Database.printAllData();
+    }
+}
